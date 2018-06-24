ba07c5da2ad171bd51090806badd42878f8738e2
monsterkodi <monsterkodi@gmx.net>
2018-06-24 18:15:06 +0200

    build

--- bin/buildwin.sh
+++ bin/buildwin.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 cd `dirname $0`/..
 
-rm -r uniko-win32-x64
+if rm -rf uniko-win32-x64; then
 
-konrad
+    konrad
+    
+    node_modules/.bin/electron-rebuild
+    
+    IGNORE="/(.*\.dmg$|Icon$|watch$|coffee$|icons$|.*md$|pug$|styl$|.*\.noon$|.*\.lock$|img/banner\.png)"
+    
+    node_modules/electron-packager/cli.js . --overwrite --icon=img/app.ico --ignore=$IGNORE
 
-node_modules/.bin/electron-rebuild
-
-IGNORE="/(.*\.dmg$|Icon$|watch$|coffee$|icons$|.*md$|pug$|styl$|.*\.noon$|.*\.lock$|img/banner\.png)"
-
-node_modules/electron-packager/cli.js . --overwrite --icon=img/uniko.ico --no-prune --ignore=$IGNORE--win32metadata.FileDescription=uniko
-
+fi
